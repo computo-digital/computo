7dd529d0546f3b8b2736437c5236bb08883e8d63
computo-digital <98721339+computo-digital@users.noreply.github.com>
2022-01-31 17:28:52 +0800

    added angular/fire

--- computo/src/environments/environment.prod.ts
+++ computo/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'computo-9fe05',
+    appId: '1:814487007366:web:67d4480f3cb682b2d5a107',
+    storageBucket: 'computo-9fe05.appspot.com',
+    apiKey: 'AIzaSyDLqYABqcOU3ZkyU_QW-3_ZuEjm7-sv8t8',
+    authDomain: 'computo-9fe05.firebaseapp.com',
+    messagingSenderId: '814487007366',
+    measurementId: 'G-2LQHSYFYES',
+  },
   production: true
 };
--- computo/src/environments/environment.ts
+++ computo/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'computo-9fe05',
+    appId: '1:814487007366:web:67d4480f3cb682b2d5a107',
+    storageBucket: 'computo-9fe05.appspot.com',
+    apiKey: 'AIzaSyDLqYABqcOU3ZkyU_QW-3_ZuEjm7-sv8t8',
+    authDomain: 'computo-9fe05.firebaseapp.com',
+    messagingSenderId: '814487007366',
+    measurementId: 'G-2LQHSYFYES',
+  },
   production: false
 };
 
