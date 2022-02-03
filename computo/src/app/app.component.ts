c683ee5dadadf78f85ed2c973f5281b30356f332
computo-digital <98721339+computo-digital@users.noreply.github.com>
2022-02-03 12:57:12 +0800

    basic fireform working.

--- computo/src/app/app.component.ts
+++ computo/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection , CollectionReference} from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, CollectionReference } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { FormBuilder, Validators, AbstractControl, ValidatorFn, ValidationErrors, AsyncValidatorFn, FormControl } from '@angular/forms';
 
-export interface EquipmentClass { ID: string; Description: string;}
-export interface Equipment { ID: string; Description: string; EquipmentClass: EquipmentClass; }
+export interface EquipmentClass { ID: string; Description: string; }
+export interface Equipment { id: string; description: string; }
 
 @Component({
   selector: 'app-root',
@@ -11,23 +12,57 @@ export interface Equipment { ID: string; Description: string; EquipmentClass: Eq
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   title = 'computo';
   private equipmentClassCollection: AngularFirestoreCollection<EquipmentClass>;
   private equipmentCollection: AngularFirestoreCollection<Equipment>;
+  public formGroup: any;
 
   equipment: Observable<Equipment[]>;
-  
-  constructor(private readonly store: AngularFirestore) {
+
+  constructor(private readonly store: AngularFirestore, private formBuilder: FormBuilder) {
     this.equipmentClassCollection = store.collection<EquipmentClass>('EquipmentClass');
     this.equipmentCollection = store.collection<Equipment>('Equipment');
-    this.equipment = this.equipmentCollection.valueChanges({ idField: 'ID' });
+    this.equipment = this.equipmentCollection.valueChanges({ idField: 'id' });
+
+    this.formGroup = this.formBuilder.group({
+      id: [''],
+      description: [''],
+      x: ['']
+      // ID: ['', Validators.required, this.objectIdExists()],
+      // Created: [new Date()],
+      // Modified: [],
+      // Description: [''],
+      // HierarchyScope: [''],
+      // EquipmentLevel: [''],
+      // SpatialDefinition: [''],
+      // EquipmentAssetMapping: [[]],
+      // OperationalLocation: [[]],
+      // EquipmentProperty: [[]],
+      // Equipment: [[]],
+      // EquipmentClassID: [[]],
+      // EquipmentCapabilityTestSpecificationID: [[]]
+    });
+    
+    
 
   }
-  addEquipment(Description: string, EquipmentClass: EquipmentClass) {
-    // Persist a document id
-    const ID = this.store.createId();
-    const item: Equipment = { ID, Description, EquipmentClass};
-    this.equipmentCollection.doc(ID).set(item);
+
+  public create() {
+    // const description = this.form.controls['description'] as string;
+
+    // // Persist a document id
+    const id = this.store.createId();
+    this.formGroup.controls['id'] = id;
+    const item: Equipment = this.formGroup.value;
+    console.log(item)
+
+    this.equipmentCollection.doc(id).set(item);
   }
+
+  public delete(id: string) {
+    // const description = this.form.controls['description'] as string;
+    this.equipmentCollection.doc(id).delete();
+  }
+
 }
--- computo/src/app/app.module.ts
+++ computo/src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig, getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../app/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     providePerformance(() => getPerformance()),
     provideRemoteConfig(() => getRemoteConfig()),
     provideStorage(() => getStorage()),
-    BrowserAnimationsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [
     ScreenTrackingService,UserTrackingService
